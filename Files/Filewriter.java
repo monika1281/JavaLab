c19de1be20201d68157c841be7606eecfabfe47c
monika1281 <120377661+monika1281@users.noreply.github.com>
2024-06-03 12:13:00 +0530

    Update Filewriter.java

--- Files/Filewriter.java
+++ Files/Filewriter.java
@@ -15,3 +15,39 @@ class L4Q1{
 Output:
 Hello World
 Files
+
+//2)Write a program to read text from a file using FileReader. (Read from a File)
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+class L4Q2{
+	public static void main(String[] args) throws IOException{
+		FileReader fr = new FileReader("abc.txt");
+		int i ;
+		while((i=fr.read())!=-1){
+			System.out.println((char)i);
+			}
+		}
+			
+}
+Output:
+H
+e
+l
+l
+o
+ 
+W
+o
+r
+l
+d
+
+
+F
+i
+l
+e
+s
+// 
+
