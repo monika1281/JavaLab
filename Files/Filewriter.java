abbadf17721a060094a1596f75dac358cb8fa354
monika1281 <120377661+monika1281@users.noreply.github.com>
2024-06-13 15:14:53 +0530

    Update Filewriter.java

--- Files/Filewriter.java
+++ Files/Filewriter.java
@@ -50,4 +50,33 @@ l
 e
 s
 // 
+//3)Write a program to copy the contents of one file to another using FileInputStream and
+FileOutputStream. (Copy a File)
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class CopyFile {
+
+   public static void main(String args[]) throws IOException {  
+      FileInputStream in = null;
+      FileOutputStream out = null;
+
+      try {
+         in = new FileInputStream("input.txt");
+         out = new FileOutputStream("output.txt");
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+   }
+}
